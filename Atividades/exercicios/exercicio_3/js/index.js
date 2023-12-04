26ba64309e9623a4a8f15fe5a44db5496dcad195
GuiNeves00 <guineves.dev@gmail.com>
2023-12-04 14:33:20 -0300

    exercicio_3 feito

---
+++ Atividades/exercicios/exercicio_3/js/index.js
@@ -0,0 +1,30 @@
+// Atualiza o seu contador a cada 1 segundo
+const countDown = setInterval(function () {
+  // Data para contagem regressiva
+  let countDownDate = new Date("Dec 5, 2023").getTime();
+  console.log("countDownDate: " + countDownDate);
+
+  // Encontrando o horário atual
+  let now = new Date().getTime();
+  console.log("now: " + now);
+
+  // Encontrando a distância entre as datas
+  let distance = countDownDate - now;
+  console.log("distance: " + distance);
+
+  if (distance > 0) {
+    //     // Calculando os dias/horas/minutos/segundos
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    //     // Insira os suas variáveis no html (use os ids já presentes no HTML)
+    document.getElementById("countdown-day").innerText = days;
+    document.getElementById("countdown-hours").innerText = hours;
+    document.getElementById("countdown-minutes").innerText = minutes;
+    document.getElementById("countdown-seconds").innerText = seconds;
+  }
+}, 1000);
